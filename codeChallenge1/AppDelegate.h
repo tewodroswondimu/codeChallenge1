c7f9ca517acb5546f14efb4721b3f44403f02523
Tewodros Wondimu <tewodros.wr@gmail.com>
2015-01-09 09:14:01 -0800

    Initial Commit

---
+++ codeChallenge1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  codeChallenge1
+//
+//  Created by Tewodros Wondimu on 1/9/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ codeChallenge1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  codeChallenge1
+//
+//  Created by Tewodros Wondimu on 1/9/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
